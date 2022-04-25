50740422f2b4a510635957a11ab0a1149021e31f
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2022-04-25 15:34:37 -0300

    Seção 9: Serviços, Http e Observable

--- meu-primeiro-app/src/app/app.component.ts
+++ meu-primeiro-app/src/app/app.component.ts
@@ -17,11 +17,15 @@ import { Component, OnInit } from '@angular/core';
   <br>
   <button (click)="add()">Add</button> -->
 
-  <ng-template [ngIf]="getDados">
+  <!-- <ng-template [ngIf]="getDados">
     <h1>{{getDados.nome}}</h1>
     <h2>{{getDados.idade}}</h2>
   </ng-template>
-  <app-output (enviarDados)="setDados($event)"></app-output>
+  <app-output (enviarDados)="setDados($event)"></app-output> -->
+
+  <app-food-add></app-food-add>
+  <app-food-list></app-food-list>
+
 
   <router-outlet></router-outlet>`
 })
--- meu-primeiro-app/src/app/shared/shared.module.ts
+++ meu-primeiro-app/src/app/shared/shared.module.ts
@@ -1,8 +1,14 @@
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
+//Components
 import { NewComponentComponent } from './new-component/new-component.component';
 import { InputComponent } from './input/input.component';
 import { OutputComponent } from './output/output.component';
+import { FoodListComponent } from './food-list/food-list.component';
+import { FoodAddComponent } from './food-add/food-add.component';
 
 
 
@@ -11,14 +17,20 @@ import { OutputComponent } from './output/output.component';
     NewComponentComponent,
     InputComponent,
     OutputComponent,
+    FoodListComponent,
+    FoodAddComponent,
   ],
   exports: [
     NewComponentComponent,
     InputComponent,
-    OutputComponent
+    OutputComponent,
+    FoodListComponent,
+    FoodAddComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule,
+    FormsModule,
   ]
 })
 export class SharedModule { }
